659e137b114813beebd407c610dbe1007c0c3e30
slonegd <slonkdv@mail.ru>
2019-08-17 08:42:36 +0300

    simple tcp server for tests

---
+++ example/tcpserver/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	ip := flag.String("ip", "", "ip for listen")
+	port := flag.Int("port", 4242, "port for listen")
+	flag.Parse()
+
+	address := fmt.Sprintf("%s:%d", *ip, *port)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		connect, err := listener.Accept()
+		if err != nil {
+			connect.Close()
+			continue
+		}
+		go handle(connect)
+	}
+}
+
+func handle(connect net.Conn) {
+	scanner := bufio.NewScanner(connect)
+	for scanner.Scan() {
+		data := scanner.Text()
+		log.Println(data)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
