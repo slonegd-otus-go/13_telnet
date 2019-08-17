82d800511f276ec98ba9fb9fe59cd5102e19aa84
slonegd <slonkdv@mail.ru>
2019-08-17 09:35:53 +0300

    non blocking client with timeout

---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
+	"time"
+)
+
+func unused(interface{}) {}
+
+func main() {
+	dialer := &net.Dialer{}
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	connect, err := dialer.DialContext(ctx, "tcp", "127.0.0.1:4242")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		read(ctx, connect)
+		wg.Done()
+	}()
+	wg.Add(1)
+	go func() {
+		write(ctx, connect)
+		wg.Done()
+	}()
+
+	wg.Wait()
+	connect.Close()
+
+	unused(cancel)
+}
+
+type scanner struct {
+	reader io.Reader
+	text   chan (string)
+}
+
+func newScanner(reader io.Reader) scanner {
+	return scanner{reader, make(chan (string))}
+}
+
+func (s scanner) Run() {
+	scanner := bufio.NewScanner(s.reader)
+	for scanner.Scan() {
+		data := scanner.Text()
+		s.text <- data
+	}
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
+
+func read(ctx context.Context, connect net.Conn) {
+	scanner := newScanner(connect)
+	go scanner.Run()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case text := <-scanner.text:
+			log.Println(text)
+		}
+	}
+}
+
+func write(ctx context.Context, connect net.Conn) {
+	scanner := newScanner(os.Stdin)
+	go scanner.Run()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case text := <-scanner.text:
+			text = fmt.Sprintf("%s\n", text)
+			_, err := connect.Write([]byte(text))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+}
